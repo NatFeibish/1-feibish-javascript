37bf73c9900b07855a4816e0d007dddd5ab49ed1
Nat <feibishn@gmail.com>
2024-06-07 11:33:17 -0500

    Added comments to JS

--- js/script.js
+++ js/script.js
@@ -52,20 +52,46 @@ const quotes = [
  * `getRandomQuote` function
 ***/
 
-//create function
-//access array
-//check for citation
-//check for year
-//create random number between 0 and 4
-//refreshes at regular intervals
-//background changes to random color
+function getRandomQuote() {
+  // 1. Create a variable that generates a random number
+  // between zero and the last index in the `quotes` array
 
+  // 2. Use the random number variable and bracket notation 
+  // to grab a random object from the `quotes` array, and 
+  // store it in a variable
+
+  // 3. Return the variable storing the random quote object
+}
 
 /***
  * `printQuote` function
 ***/
 
+function printQuote() {
+  // 1. Create a variable that calls the getRandomQuote() 
+  // function
 
+  // 2. Create a variable that initiates your HTML string with 
+  // the first two <p></p> elements, their classNames, 
+  // and the quote and source properties, but leave off 
+  // the second closing `</p>` tag for now
+
+  // 3. Use an if statement to check if the citation property 
+  // exists, and if it does, concatenate a <span></span> 
+  // element, appropriate className, and citation property 
+  // to the HTML string
+
+  // 4. Use an if statement to check of the year property exists, 
+  // and if it does, concatenate a <span></span> element, 
+  // appropriate className, and year property to the HTML 
+  //string
+
+  // 5. After the two if statements, concatenate the closing </p> 
+  // tag to the HTML string
+
+  // 6. set the innerHTML of the quote-box div to equal the 
+  // complete HTML string
+}
 
 /***
  * click event listener for the print quote button
